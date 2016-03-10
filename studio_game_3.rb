113eafed100e10c70fe2b42dfbcc66f07fee1c8b
bmansk8 <bmansk8@yahoo.com>
2016-03-10 09:44:07 -0500

    third version of my game

---
+++ studio_game_3.rb
@@ -0,0 +1,10 @@
+player = "larry"
+health = 60
+
+puts "#{player}'s health is #{health * 3}"
+
+#puts "#{player}'s health is #{health / 3.0}"
+# 3.0 for decimal/float
+
+#puts "#{player}'s health is #{health / 3}"
+# 3 for int
