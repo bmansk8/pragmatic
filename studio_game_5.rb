cf7090021e778923bf757fe5e1422abca7333c50
bmansk8 <bmansk8@yahoo.com>
2016-03-10 09:45:25 -0500

    fith version of my game

---
+++ studio_game_5.rb
@@ -0,0 +1,7 @@
+#OLD WAY puts "players: \n \t larry \n \t curly \n \t moe"
+
+player0 = "larry"
+player1 = "curly"
+player2 = "moe"
+
+puts "players : \n\t #{player0} \n\t #{player1} \n\t #{player2}"
