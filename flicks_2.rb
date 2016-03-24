2c124212688796d214ce18b2d6d758157e3bc823
bmansk8 <bmansk8@yahoo.com>
2016-03-24 10:59:21 -0400

    our flicks program

---
+++ flicks_2.rb
@@ -0,0 +1,11 @@
+title_0 = "goonies"
+title_1 = "ghostbusters"
+title_2 = "goldfinger"
+
+rank_0 = 10
+rank_1 = 9
+rank_2 = 8
+
+puts "#{title_0.capitalize} has a rank of #{rank_0}"
+puts "#{title_1.capitalize} has a rank of #{rank_1}"
+puts "#{title_2.capitalize} has a rank of #{rank_2}"
