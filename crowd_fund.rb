2040621f7712444ee9ef05b9bbbecbd71e485fb0
bmansk8 <bmansk8@yahoo.com>
2016-03-29 10:44:28 -0400

    crowd fund v2

--- crowd_fund.rb
+++ crowd_fund.rb
@@ -1,20 +1,54 @@
-project0 = "math"
-project1 = "programming"
-project2 = "music"
+class Crowd_Fund
 
-funds0 = 400
-funds1 = 0
-funds2 = 600
+    def initialize(subject,amount=100,goal=105)
+        @subject = subject
+        @amount = amount
+        @goal = goal
+    end
+
+    def to_s
+        "project #{@subject} has $#{@amount} of funds and has a goal of#{@goal}"
+    end
+
+    def gain_funds
+        @amount +=100
+        "project #{@subject} now has #{@amount} dollars of funds"
+    end
+
+    def loose_funds
+        @amount -=100
+        "project #{@subject} now has #{@amount} dollars of funds"
+    end
+
+end
 
 current_time = Time.new
 formatted_time = current_time.strftime("%A %B/%d/%Y at %I:%M%p")
 puts "funding check started at #{formatted_time}"
 
 
-def funds_check(subject,amount=0)
-  puts "project #{subject.ljust(30, '.')} has $#{amount} of funds"
+project0_name = "math"
+project1_name = "programming"
+project2_name = "music"
+
+funds0 = 400
+funds1 = 0
+funds2 = 600
+
+goal0 = 400
+goal1 = 1000
+goal2 = 2500
+
+project0= Crowd_Fund.new(project0_name,funds0,goal0)
+project1= Crowd_Fund.new(project1_name,funds1,goal1)
+project2= Crowd_Fund.new(project2_name,funds2,goal2)
+
+puts project0
+puts project1
+puts project2
+
+3.times do
+project2.gain_funds
 end
 
-puts funds_check(project0,funds0)
-puts funds_check(project1)
-puts funds_check(project2,funds2)
+project2.loose_funds
